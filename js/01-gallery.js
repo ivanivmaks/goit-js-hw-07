8522b051630268ba9d1db855176a070c0d7a9d9a
ivaniv_maks <ivanivmaks0@gmail.com>
2023-06-08 01:20:20 +0300

    updated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,19 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 const gallery = document.querySelector(".gallery");
-// Створення і рендер розмітки на підставі масиву даних galleryItems
 function createGallery() {
   const items = galleryItems.map((item) => createGalleryItem(item));
   gallery.append(...items);
 }
-// Створення розмітки елемента галереї
+
 function createGalleryItem({ preview, original, description }) {
-  // створення li
   const galleryItem = document.createElement("li");
   galleryItem.classList.add("gallery__item");
-  // стврорення а
+
   const link = document.createElement("a");
   link.classList.add("gallery__link");
   link.href = original;
   link.addEventListener("click", openImg);
-  // створення img
+
   const img = document.createElement("img");
   img.classList.add("gallery__image");
   img.src = preview;
@@ -24,24 +21,21 @@ function createGalleryItem({ preview, original, description }) {
   img.alt = description;
   img.addEventListener("click", preventDefault);
 
-  // ствоення вкладеності
-  link.appendChild(img);
   galleryItem.appendChild(link);
+  link.appendChild(img);
 
   return galleryItem;
 }
-// Заборона перенаправлення за замовчуванням
+
+createGallery();
+
 function preventDefault(event) {
   event.preventDefault();
 }
-// Реалізація делегування на ul.gallery і отримання url великого зображення
+
 function openImg(event) {
   event.preventDefault();
-  const largeUrl = event.target.dataset.source;
-  openLightbox(largeUrl);
-}
-// Відкриття модального вікна з великим зображенням
-function openLightbox(url) {
+  const url = event.target.dataset.source;
   const instance = basicLightbox.create(`<img src="${url}">`, {
     onShow: handleShow,
     onClose: handleClose,
@@ -56,12 +50,9 @@ function openLightbox(url) {
     document.removeEventListener("keydown", handleKeyDown);
   }
 
-  // закриття клавішою "Escape"
   function handleKeyDown(event) {
     if (event.code === "Escape") {
       instance.close();
     }
   }
 }
-createGallery();
-console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,4 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 const gallery = document.querySelector(".gallery");
 function createGallery() {
   const items = galleryItems.map((item) => createGalleryItem(item));
@@ -8,26 +7,26 @@ function createGallery() {
 
 function createGalleryItem({ preview, original, description }) {
   const galleryItem = document.createElement("li");
-  gallery.classList.add("gallery__item");
+  galleryItem.classList.add("gallery__item");
+
   const link = document.createElement("a");
-  gallery.classList.add("gallery_link");
+  link.classList.add("gallery__link");
   link.href = original;
+
   const img = document.createElement("img");
   img.classList.add("gallery__image");
   img.src = preview;
   img.alt = description;
 
-  link.appendChild(img);
   galleryItem.appendChild(link);
+  link.appendChild(img);
 
   return galleryItem;
 }
 
 createGallery();
 
-const lightBox = new SimpleLightbox(".gallery a", {
+const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionsDelay: "250ms",
 });
-
-console.log(galleryItems);
